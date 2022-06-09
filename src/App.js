9c685b03c791d614e2a5c493b26417e16c3cb8c9
prevaldas <prevaldas@yahoo.com>
2022-06-09 17:45:05 +0100

    asignment completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import React, { Component } from "react";
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
+    <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
